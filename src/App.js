7233a5baee0efc343f3369292a0008c84b6e2903
Tim Ryan <tim@timryan.dev>
2020-10-24 07:59:20 -0400

    updated app.js for travis test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to restart. Yuppers.
+          Edit <code>src/App.js</code> and save to restart. Yuppers!
         </p>
         <a
           className="App-link"
